6bc3570d3fcc4ec4d1fb2b774ec7bff27499876b
Muhammad3111 <karimovmuhammad3502@gmail.com>
2024-04-10 00:53:40 +0500

    change apiClient.ts of frontend folder

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -6,11 +6,7 @@ import { ProductTypes } from "./components/products/AddProduct";
 import { ExpenseTypes } from "./components/expenses/AddExpense";
 
 export const register = async () => {
-  const response = await instance.post(
-    `users/register`,
-    {},
-    { withCredentials: true }
-  );
+  const response = await instance.post(`users/register`, {});
 
   const responseBody = await response.data;
 
@@ -21,7 +17,6 @@ export const register = async () => {
 
 export const signIn = async (formData: FormTypes) => {
   const response = await instance.post(`auth/login`, formData, {
-    withCredentials: true,
     headers: {
       "Content-Type": "application/json",
     },
@@ -30,25 +25,17 @@ export const signIn = async (formData: FormTypes) => {
 };
 
 export const validateToken = async () => {
-  const response = await instance.get(`/auth/validate-token`, {
-    withCredentials: true,
-  });
+  const response = await instance.get(`/auth/validate-token`);
   return response;
 };
 
 export const signOut = async () => {
-  const response = await instance.post(
-    `/auth/logout`,
-    {},
-    { withCredentials: true }
-  );
+  const response = await instance.post(`/auth/logout`);
   return response.data;
 };
 
 export const report = async () => {
-  const response = await instance.get("routes/report", {
-    withCredentials: true,
-  });
+  const response = await instance.get("routes/report");
   return (
     response.data || {
       count: 0,
@@ -62,102 +49,72 @@ export const report = async () => {
 };
 
 export const addCategory = async ({ title }: CategoryTypes) => {
-  const response = await instance.post(
-    "routes/category",
-    { title },
-    { withCredentials: true }
-  );
+  const response = await instance.post("routes/category", { title });
   return response.data;
 };
 
 export const getCategory = async () => {
-  const response = await instance.get("routes/category", {
-    withCredentials: true,
-  });
+  const response = await instance.get("routes/category");
   return response.data;
 };
 
 export const deleteCategory = async (id: string) => {
-  const response = await instance.delete(`routes/category/${id}`, {
-    withCredentials: true,
-  });
+  const response = await instance.delete(`routes/category/${id}`);
   return response;
 };
 
 export const updateCategory = async ({ _id, title }: UpdateCategoryTypes) => {
-  const response = await instance.put(
-    `routes/category/${_id}`,
-    { title },
-    { withCredentials: true }
-  );
+  const response = await instance.put(`routes/category/${_id}`, { title });
   return response.data;
 };
 
 export const getProducts = async () => {
-  const response = await instance.get("routes/products", {
-    withCredentials: true,
-  });
+  const response = await instance.get("routes/products");
   return response.data;
 };
 
 export const addProduct = async (formData: ProductTypes) => {
-  const response = await instance.post("routes/product", formData, {
-    withCredentials: true,
-  });
+  const response = await instance.post("routes/product", formData);
   return response.data;
 };
 
 export const updateProduct = async (formData: UpdateCategoryTypes) => {
   const response = await instance.put(
     `routes/product/${formData._id}`,
-    formData,
-    {
-      withCredentials: true,
-    }
+    formData
   );
   return response.data;
 };
 
 export const deletedProduct = async (id: string) => {
-  const response = await instance.delete(`routes/product/${id}`, {
-    withCredentials: true,
-  });
+  const response = await instance.delete(`routes/product/${id}`);
   return response;
 };
 
 export const getSoldProducts = async () => {
-  const response = await instance.get("routes/sold-products", {
-    withCredentials: true,
-  });
+  const response = await instance.get("routes/sold-products");
   return response.data;
 };
 
 export const getExpenses = async () => {
-  const response = await instance.get("routes/expense", {
-    withCredentials: true,
-  });
+  const response = await instance.get("routes/expense");
   return response.data;
 };
 
 export const addExpense = async (formData: ExpenseTypes) => {
-  const response = await instance.post("routes/expense", formData, {
-    withCredentials: true,
-  });
+  const response = await instance.post("routes/expense", formData);
   return response;
 };
 
 export const deleteExpense = async (id: string) => {
-  const response = await instance.delete(`routes/expense/${id}`, {
-    withCredentials: true,
-  });
+  const response = await instance.delete(`routes/expense/${id}`);
   return response;
 };
 
 export const updateExpense = async (formData: ExpenseTypes) => {
   const response = await instance.put(
     `routes/expense/${formData._id}`,
-    formData,
-    { withCredentials: true }
+    formData
   );
   return response;
 };
