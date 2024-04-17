e67117fb11a93f5c4dd3f91fa5e415412b285110
Muhammad3111 <karimovmuhammad3502@gmail.com>
2024-04-17 10:27:20 +0500

    add loged check

--- frontend/src/PrivateRoute.tsx
+++ frontend/src/PrivateRoute.tsx
@@ -1,6 +1,8 @@
 // import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useAppContext } from "./context/AuthContext";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 // import { toast } from "react-toastify";
 
 type PrivateRouteProps = {
@@ -10,11 +12,11 @@ type PrivateRouteProps = {
 export default function PrivateRoute({ children }: PrivateRouteProps) {
   const { isLoggedIn } = useAppContext();
 
-  // useEffect(() => {
-  //   if (!isLoggedIn) {
-  //     toast.error("Foydalanuvchi tizimga kirmagan");
-  //   }
-  // }, [isLoggedIn]);
+  useEffect(() => {
+    if (!isLoggedIn) {
+      toast.error("Foydalanuvchi tizimga kirmagan");
+    }
+  }, [isLoggedIn]);
   // isLoggedIn false bo'lsa Login sahifasiga otadi
   return isLoggedIn ? children : <Navigate to="/" replace />;
 }
