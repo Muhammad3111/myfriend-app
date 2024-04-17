65dda6e25e93f8eac3f77c308b52a3be6690faef
Muhammad3111 <karimovmuhammad3502@gmail.com>
2024-04-17 13:01:51 +0500

    update files

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://dilmurod-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://d3280.vercel.app"],
     credentials: true,
   })
 );
